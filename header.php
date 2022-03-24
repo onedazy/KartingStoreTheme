3efd3b0f2d01d8ff485e95aa8e78e61ca5711cf6
Koen van Ginkel <info@appicsoftware.com>
2022-03-24 05:52:33 -0400

    added footer fixed some issues

--- header.php
+++ header.php
@@ -61,7 +61,7 @@
 						</ul>
 					</li>
 					<div>
-						<a class="nav-item login" href="/my-account">inloggen</a>
+						<a class="nav-item login" href="/my-account"><?php  if(is_user_logged_in() == true ): echo 'mijn account'; endif; if(is_user_logged_in() == false ): echo 'inloggen'; endif; ?></a>
 						<a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>">
 							<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart-dash-fill" viewBox="0 0 16 16">
 								<path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM6.5 7h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1 0-1z"/>
